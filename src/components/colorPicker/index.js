be59acef4820e4e3624c93d60a5529bc75e3bbb4
heathshults <heatheshults@gmail.com>
2023-12-06 15:42:53 -0600

    test

--- src/components/colorPicker/index.js
+++ src/components/colorPicker/index.js
@@ -3,18 +3,23 @@ import "./index.css";
 const classNames = require('classnames');
 
 export default class ColorPicker extends Component {
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedColor: this.props.initialSelectedColor,
+    };
+  }
   render() {
-    let selectedColor;
+    let selectedColor = this.props.initialSelectedColor;
     return (
       <div>
         <div className="layout-row justify-content-center">
           <div className="card mt-75">
-            <div className="canvas" data-testid="selectedColor">
+            <div className="canvas" data-testid="selectedColor"  style={{ backgroundColor: this.state.selectedColor}} >
               <p className="text-center mx-auto px-5">{selectedColor}</p>
             </div>
             <div className="card-actions">
-              <div className="layout-row justify-content-center align-items-center" data-testid="colorPickerOptions">
+              <div className="layout-row justify-content-center align-items-center"data-testid="colorPickerOptions">
                 {this.props.colorPickerOptions.map((color, index) => {
                   return (
                     <div
@@ -27,6 +32,10 @@ export default class ColorPicker extends Component {
                         })
                       }
                       key={color}
+                      style={{ backgroundColor: color, cursor: "pointer" }}
+                      onClick={() => {
+                        this.setState({selectedColor: color});
+                      }}
                     ></div>
                   );
                 })}
